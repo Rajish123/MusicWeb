f2f49d66010a530446b9ad6697ce7ce882b32b45
rajish <maharjanrajish101@gmail.com>
2022-11-02 20:31:22 +0545

    album song functions

--- music/views.py
+++ music/views.py
@@ -26,5 +26,17 @@ def ArtistAlbums(request,id,slug):
     album = artists.albums.all().order_by('-votes')
     context = {'artists':artists, 'albums':album}
     return render(request,'music/artist_albums.html',context)
+
+# get all songs of album
+def AlbumSongs(request,album_id,slug):
+    context = {}
+    albums = get_object_or_404(
+        Album,
+        album_id = album_id,
+        slug = slug
+    )
+    songs = albums.songs.all().order_by('-date_posted')
+    context = {'albums':albums, 'songs':songs}
+    return render(request,'music/album_songs.html',context)
     
 
