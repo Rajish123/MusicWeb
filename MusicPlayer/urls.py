837b76e4c8a9d347893e1c9aa9d5d982980a0048
rajish <maharjanrajish101@gmail.com>
2022-11-08 21:08:19 +0545

    new url

--- MusicPlayer/urls.py
+++ MusicPlayer/urls.py
@@ -20,7 +20,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include("music.urls"))
+    path('',include("music.urls")),
+    path('accounts/',include("accounts.urls"))
 ]
 
 if settings.DEBUG:
