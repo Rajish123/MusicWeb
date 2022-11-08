34015d5b97724db620b44105320706aec8d27096
rajish <maharjanrajish101@gmail.com>
2022-11-08 21:09:10 +0545

    signup&login

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import login,authenticate
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+
+def Signup(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        print(form.data)
+        if form.is_valid():
+            form.save()
+            data = form.cleaned_data.get("form_field")
+            print(data)
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request,user)
+    else:
+        
+        form = UserCreationForm()
+        # print("not vlaid")
+    return render(request,'accounts/signup.html',{'form':form})
