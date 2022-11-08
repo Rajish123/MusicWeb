b3f36c1033997c88a7c16889922384901e9c4526
rajish <maharjanrajish101@gmail.com>
2022-11-08 21:08:41 +0545

    profile model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save  
+from django.dispatch import receiver
+from PIL import Image
 
-# Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default = 'default.jpg', upload_to = 'profile_picture')
+
+    def __str__(self):
+        return f"{self.user.username} Profile"
+
+    def save(self,*args,**kwargs):
+        super().save(*args,**kwargs)
+        img = Image.open(self.image.path)
+        if img.height > 200 or img.width > 200:
+            output_size = (491,657)
+            # resize previous image
+            img.thumbnail(output_size)  
+            # override previous image
+            img.save(self.image.path)
+
+# whenever user instances are created,automatically profile model are created
+@receiver(post_save, sender = User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user = instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender,instance, **kwargs):
+    instance.profile.save()
+
